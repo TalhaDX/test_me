9d68a8ee79424ac9b5c8b8d08627ae46b9065c0e
Muhammad Talha <talha4492@gmail.com>
2025-07-30 21:16:25 +0300

    Another update

--- main.py
+++ main.py
@@ -71,3 +71,10 @@ def read_items():
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         db.close()
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
